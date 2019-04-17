b3fe7b8cfba26f5e002191d97015b8f7dbccae68
Trifon <trifon.yanakiev@gmail.com>
2019-04-17 18:09:04 -0500

    Adding css and images for layout page

---
+++ AnglerNet/Models/Wall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnglerNet.Models
+{
+    public partial class Wall
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Content { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        public AspNetUsers User { get; set; }
+    }
+}
